b3ec92cc7c33b06c78efe185308d212a776a61a8
PleaBanshee <llewellynant@gmail.com>
2020-09-28 17:27:27 +0200

    Added connectionstring to Project

--- Cinemagic/Cinemagic/Form1.cs
+++ Cinemagic/Cinemagic/Form1.cs
@@ -7,14 +7,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Cinemagic
 {
     public partial class Form1 : Form
     {
+        public SqlCommand com;
+        public SqlConnection conn;
+        public DataSet ds;
+        public SqlDataAdapter adap;
+        public string constr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\CinemagicDB.mdf;Integrated Security=True;";
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                conn = new SqlConnection(constr);
+                conn.Open();
+                MessageBox.Show("Connection Successfull");
+                conn.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Could not connect to db");
+;            }
+        }
     }
 }
+           
+
