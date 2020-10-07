d4abcfd18ca878d07f836fac0e70fe9c255393c2
Roestenburg-J <65230144+Roestenburg-J@users.noreply.github.com>
2020-10-07 10:23:31 +0200

    Added Dynamic 'addCustomer' form

--- Cinemagic/Cinemagic/frmCustomer.Designer.cs
+++ Cinemagic/Cinemagic/frmCustomer.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Cinemagic
 {
-    partial class frmCustomer
+    partial class Customer
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnAddCustomer = new System.Windows.Forms.Button();
+            this.btnEditCustomer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAddCustomer
+            // 
+            this.btnAddCustomer.Location = new System.Drawing.Point(114, 226);
+            this.btnAddCustomer.Name = "btnAddCustomer";
+            this.btnAddCustomer.Size = new System.Drawing.Size(102, 43);
+            this.btnAddCustomer.TabIndex = 0;
+            this.btnAddCustomer.Text = "Add Customer";
+            this.btnAddCustomer.UseVisualStyleBackColor = true;
+            this.btnAddCustomer.Click += new System.EventHandler(this.btnAddCustomer_Click);
+            // 
+            // btnEditCustomer
+            // 
+            this.btnEditCustomer.Location = new System.Drawing.Point(449, 226);
+            this.btnEditCustomer.Name = "btnEditCustomer";
+            this.btnEditCustomer.Size = new System.Drawing.Size(75, 23);
+            this.btnEditCustomer.TabIndex = 1;
+            this.btnEditCustomer.Text = "button2";
+            this.btnEditCustomer.UseVisualStyleBackColor = true;
+            // 
+            // Customer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmCustomer";
+            this.Controls.Add(this.btnEditCustomer);
+            this.Controls.Add(this.btnAddCustomer);
+            this.Name = "Customer";
+            this.Text = "Customers";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnAddCustomer;
+        private System.Windows.Forms.Button btnEditCustomer;
     }
 }
--- Cinemagic/Cinemagic/frmCustomer.cs
+++ Cinemagic/Cinemagic/frmCustomer.cs
@@ -7,14 +7,85 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace Cinemagic
 {
-    public partial class frmCustomer : Form
+    public partial class Customer : Form
     {
-        public frmCustomer()
+        public Customer()
         {
             InitializeComponent();
+        }
+
+
+        public static DialogResult AddCustomer(string title, string promptText)
+        {
+            Form form = new Form();
+
+            Label lblID = new Label();
+            Label lblName = new Label();
+            Label lblSurname = new Label();
+            Label lblEmail = new Label();
+            Label lblPhone = new Label(); 
+
+            TextBox txtName = new TextBox();
+            TextBox txtSurname = new TextBox();
+            TextBox txtID = new TextBox();
+            TextBox txtEmail = new TextBox();
+            TextBox txtPhone = new TextBox();
+
+
+            Button btnAdd = new Button();
+            Button btnCancel = new Button();
+            btnAdd.DialogResult = DialogResult.OK;
+            btnCancel.DialogResult = DialogResult.Cancel;
+
+            lblID.SetBounds(10,10,100,13);
+            lblName.SetBounds(40, 10, 100, 13);
+            lblSurname.SetBounds(70, 10, 100, 13);
+            lblPhone.SetBounds(100, 10, 100, 13);
+            lblEmail.SetBounds(130, 10, 100, 13);
+
+            txtID.SetBounds(20, 10, 100, 13);
+            txtID.SetBounds(50, 10, 100, 13);
+            txtID.SetBounds(80, 10, 100, 13);
+            txtID.SetBounds(110, 10, 100, 13);
+            txtID.SetBounds(140, 10, 100, 13);
+
+            lblID.AutoSize = true;
+            lblName.AutoSize = true;
+            lblSurname.AutoSize = true;
+            lblPhone.AutoSize = true;
+            lblEmail.AutoSize = true;
+
+            txtID.Anchor = txtID.Anchor | AnchorStyles.Right;
+            txtName.Anchor = txtName.Anchor | AnchorStyles.Right;
+            txtSurname.Anchor = txtSurname.Anchor | AnchorStyles.Right;
+            txtPhone.Anchor = txtPhone.Anchor | AnchorStyles.Right;
+            txtEmail.Anchor = txtEmail.Anchor | AnchorStyles.Right;
+
+            btnAdd.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnCancel.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+
+            form.ClientSize = new Size(300, 107);
+            form.Controls.AddRange(new Control[] { lblID, lblName, lblSurname, lblPhone, lblEmail, txtID, txtName, txtSurname, txtID, txtSurname, txtEmail,btnAdd, btnCancel });
+            form.ClientSize = new Size(Math.Max(300, lblID.Right + 10), form.ClientSize.Height);
+            form.FormBorderStyle = FormBorderStyle.FixedDialog;
+            form.StartPosition = FormStartPosition.CenterScreen;
+            form.MinimizeBox = false;
+            form.MaximizeBox = false;
+            form.AcceptButton = btnAdd;
+            form.CancelButton = btnCancel;
+
+            DialogResult dialogResult = form.ShowDialog();
+            return dialogResult;
+        }
+
+        private void btnAddCustomer_Click(object sender, EventArgs e)
+        {
+            AddCustomer("customer", "add");
+           
         }
     }
 }
