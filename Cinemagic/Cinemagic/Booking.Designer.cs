09c897dd3a70204b01150f550131ef6c1ca8a29b
Roestenburg-J <65230144+Roestenburg-J@users.noreply.github.com>
2020-10-15 08:21:09 +0200

    Imported Booking Form

--- Cinemagic/Cinemagic/Booking.Designer.cs
+++ Cinemagic/Cinemagic/Booking.Designer.cs
@@ -29,12 +29,18 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btndisplay = new System.Windows.Forms.Button();
             this.txtbooking = new System.Windows.Forms.TextBox();
             this.txtcost = new System.Windows.Forms.TextBox();
             this.txtseats = new System.Windows.Forms.TextBox();
             this.dtptickets = new System.Windows.Forms.DateTimePicker();
             this.btninsert = new System.Windows.Forms.Button();
+            this.btnupdate = new System.Windows.Forms.Button();
+            this.btnbooking = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -46,64 +52,124 @@
             this.dataGridView1.Size = new System.Drawing.Size(645, 228);
             this.dataGridView1.TabIndex = 1;
             // 
-            // button2
+            // btndisplay
             // 
-            this.button2.Location = new System.Drawing.Point(329, 301);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(119, 43);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Display database";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.btndisplay.Location = new System.Drawing.Point(74, 259);
+            this.btndisplay.Name = "btndisplay";
+            this.btndisplay.Size = new System.Drawing.Size(119, 52);
+            this.btndisplay.TabIndex = 3;
+            this.btndisplay.Text = "Display Database";
+            this.btndisplay.UseVisualStyleBackColor = true;
+            this.btndisplay.Click += new System.EventHandler(this.button2_Click);
             // 
             // txtbooking
             // 
-            this.txtbooking.Location = new System.Drawing.Point(128, 389);
+            this.txtbooking.Location = new System.Drawing.Point(619, 343);
             this.txtbooking.Name = "txtbooking";
             this.txtbooking.Size = new System.Drawing.Size(100, 20);
             this.txtbooking.TabIndex = 4;
             // 
             // txtcost
             // 
-            this.txtcost.Location = new System.Drawing.Point(348, 389);
+            this.txtcost.Location = new System.Drawing.Point(192, 364);
             this.txtcost.Name = "txtcost";
             this.txtcost.Size = new System.Drawing.Size(100, 20);
             this.txtcost.TabIndex = 5;
             // 
             // txtseats
             // 
-            this.txtseats.Location = new System.Drawing.Point(515, 389);
+            this.txtseats.Location = new System.Drawing.Point(192, 411);
             this.txtseats.Name = "txtseats";
             this.txtseats.Size = new System.Drawing.Size(100, 20);
             this.txtseats.TabIndex = 6;
             // 
             // dtptickets
             // 
-            this.dtptickets.Location = new System.Drawing.Point(548, 339);
+            this.dtptickets.Location = new System.Drawing.Point(192, 456);
             this.dtptickets.Name = "dtptickets";
             this.dtptickets.Size = new System.Drawing.Size(200, 20);
             this.dtptickets.TabIndex = 7;
             // 
             // btninsert
             // 
-            this.btninsert.Location = new System.Drawing.Point(232, 339);
+            this.btninsert.Location = new System.Drawing.Point(223, 259);
             this.btninsert.Name = "btninsert";
-            this.btninsert.Size = new System.Drawing.Size(91, 42);
+            this.btninsert.Size = new System.Drawing.Size(131, 52);
             this.btninsert.TabIndex = 8;
             this.btninsert.Text = "Insert";
             this.btninsert.UseVisualStyleBackColor = true;
+            // 
+            // btnupdate
+            // 
+            this.btnupdate.Location = new System.Drawing.Point(422, 259);
+            this.btnupdate.Name = "btnupdate";
+            this.btnupdate.Size = new System.Drawing.Size(128, 52);
+            this.btnupdate.TabIndex = 9;
+            this.btnupdate.Text = "Update Records";
+            this.btnupdate.UseVisualStyleBackColor = true;
+            // 
+            // btnbooking
+            // 
+            this.btnbooking.Location = new System.Drawing.Point(580, 259);
+            this.btnbooking.Name = "btnbooking";
+            this.btnbooking.Size = new System.Drawing.Size(139, 52);
+            this.btnbooking.TabIndex = 10;
+            this.btnbooking.Text = "Delete Booking";
+            this.btnbooking.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(75, 367);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Total Ticketcost:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(74, 414);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Number of seats:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(74, 462);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Ticket Sale date:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(564, 346);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Booking:";
             // 
             // Booking
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(816, 502);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnbooking);
+            this.Controls.Add(this.btnupdate);
             this.Controls.Add(this.btninsert);
             this.Controls.Add(this.dtptickets);
             this.Controls.Add(this.txtseats);
             this.Controls.Add(this.txtcost);
             this.Controls.Add(this.txtbooking);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btndisplay);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Booking";
             this.Text = "Form2";
@@ -117,11 +183,17 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btndisplay;
         private System.Windows.Forms.TextBox txtbooking;
         private System.Windows.Forms.TextBox txtcost;
         private System.Windows.Forms.TextBox txtseats;
         private System.Windows.Forms.DateTimePicker dtptickets;
         private System.Windows.Forms.Button btninsert;
+        private System.Windows.Forms.Button btnupdate;
+        private System.Windows.Forms.Button btnbooking;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
